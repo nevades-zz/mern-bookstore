1c6f54e6dbc8de41efa788252f2b9f994d9ae2ef
nevades <nevanjithdes@gmail.com>
2024-09-02 11:51:48 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { PORT, mongoDBURL } from "./config.js";
+import booksRoute from "./routes/booksRoute.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to MERN Book Store");
+});
+
+app.use("/books", booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`APP working on ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ frontend/src/main.jsx
@@ -0,0 +1,13 @@
+import App from "./App.jsx";
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <SnackbarProvider>
+      <App />
+    </SnackbarProvider>
+  </BrowserRouter>
+);
